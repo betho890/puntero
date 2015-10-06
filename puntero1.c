ef76d37c4d409855f203ba98c7ea431b1ce2816b
Diaz Ramos Carlos Alberto <carlos.diaz.sc@itszapopan.edu.mx>
2015-10-05 19:15:11 -0500

    trabajo en clase

---
+++ puntero1.c
@@ -0,0 +1,28 @@
+nclude <stdio.h> 
+int main (int argc, char const *argv[]);
+{
+int t;
+
+prinf ("%s\n", "tamaño del arreglo");
+scanf("%d\",&t);
+
+printf("le asigna valor de %d al arreglo\n",t);
+int arreglo[t];
+
+printf("%s\n", "Ahora llenamos");
+
+for (int i = 0; i<t; i++){
+  scanf("%d",&arreglo[i];
+}
+
+printf("%s\n", "ya esta lleno");
+
+for(int i = 0; i<t; ++i){
+   printf("arreglo[%d]=%d\n",i, arreglo[i]);
+}
+
+printf("listo");
+
+retunr o;
+}
+
